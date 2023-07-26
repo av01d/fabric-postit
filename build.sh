773d016dbf39f07cf59586586b43e5fd75bff5a7
wiebe <wipeautcrafter@gmail.com>
2023-07-26 09:33:52 +0200

    chore: build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+esbuild lib/fabric.postit.js --bundle --format=iife --outfile=dist/fabric.postit.js
+esbuild lib/fabric.postit.js --bundle --minify --format=iife --outfile=dist/fabric.postit.min.js
+
+copy="/* Copyright $(date +'%Y') WebGear en EvoStack. All rights reserved. */"
+echo "$copy" | cat - dist/fabric.postit.js > temp && mv temp dist/fabric.postit.js
+echo "$copy" | cat - dist/fabric.postit.min.js > temp && mv temp dist/fabric.postit.min.js
